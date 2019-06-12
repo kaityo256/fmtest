d3c586e8ce7c9be820e14eb8c2369231afb2d7e5
H. Watanabe <kaityo@users.sourceforge.jp>
2019-06-12 17:05:40 +0900

    adds 2nd

--- main.cpp
+++ main.cpp
@@ -1,7 +1,29 @@
 #include "gs.h"
+#include <fstream>
+#include <iostream>
+
+void dump(Formura_Navi &n) {
+  const int size = n.upper_x - n.lower_x;
+  char filename[256];
+  static int index = 0;
+  sprintf(filename, "data/%03d.dat", index);
+  index++;
+  std::cout << filename << std::endl;
+  std::ofstream ofs(filename);
+  for (int i = 0; i < size; i++) {
+    int i2 = (i - n.offset_x) % size;
+    double v = formura_data.u[i2];
+    ofs << i << " ";
+    ofs << v << std::endl;
+  }
+}
 
 int main(int argc, char **argv) {
   Formura_Navi n;
   Formura_Init(&argc, &argv, &n);
+  for (int i = 0; i < 100; i++) {
+    Formura_Forward(&n);
+    dump(n);
+  }
   Formura_Finalize();
 }
---
+++ offest.plt
@@ -0,0 +1,4 @@
+set term png
+set out "offset.png"
+set style data line
+p "data/000.dat","data/005.dat","data/010.dat"
